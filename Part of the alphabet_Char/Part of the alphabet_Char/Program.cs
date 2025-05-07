bb5e5b376ed623edcaa61cab5e92df7ee3d6ebda
Vlad119 <unstarkble12@gmail.com>
2025-05-07 23:43:49 +0300

    Программа выводит все строчные буквы латинского алфавита, начиная от начала до заданной буквы

---
+++ Part of the alphabet_Char/Part of the alphabet_Char/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        char c = char.Parse(Console.ReadLine());
+        for (char ch = 'a'; ch <= c; ch++)
+        {
+            Console.Write(ch + " ");
+        }
+    }
+}
---
+++ Part of the alphabet_Char/Part of the alphabet_Char/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Part of the alphabet_Char")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Part of the alphabet_Char")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("afbed20a-54d3-4f55-9c20-5e6310f28149")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
