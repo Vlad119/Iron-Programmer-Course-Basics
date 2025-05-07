2ef43fc1013fd55e91033b7f45879c6d9a7c6162
Vlad119 <unstarkble12@gmail.com>
2025-05-07 23:10:28 +0300

    Задача на палиндром

---
+++ Is Palindrome/Is Palindrome/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int original = n, reversed = 0;
+        while (n > 0)
+        {
+            reversed = reversed * 10 + n % 10;
+            n /= 10;
+        }
+        Console.WriteLine((original == reversed) ? "YES" : "NO");
+    }
+}
