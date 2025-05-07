bc5d5ca3b5fa0d7fd4be6d878bce4c776bfda0b0
Vlad119 <unstarkble12@gmail.com>
2025-05-07 22:22:07 +0300

    Задача на количество четверок среди всех цифр числа n

---
+++ Number of fours/Number of fours/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Number_of_fours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            while (n > 0)
+            {
+                if (n % 10 == 4)
+                {
+                    count++;
+                }
+                n /= 10;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
