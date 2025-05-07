736e712248b7acdceb16fda9455eeebbe7e8c0d9
Vlad119 <unstarkble12@gmail.com>
2025-05-07 22:10:51 +0300

    Задача на нахождение произведения четных чисел

---
+++ Product of even numbers/Product of even numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Product_of_even_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int mult = 1;
+            for (int i = 1; i <= N; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    mult *= i;
+                }
+            }
+            Console.WriteLine(mult);
+        }
+    }
+}
