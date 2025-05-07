6c398420d2bc5b8a3755493af3baee05facfec0a
Vlad119 <unstarkble12@gmail.com>
2025-05-07 23:17:41 +0300

    Программа находит степень числа без math.pow

---
+++ Power of a number/Power of a number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int result = 1;
+        for (int i = 0; i < b; i++)
+        {
+            result *= a;
+        }
+        Console.WriteLine(result);
+    }
+}
