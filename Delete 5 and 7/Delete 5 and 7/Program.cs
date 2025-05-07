f77d744335d469723c23704a3745c3d9cc328cee
Vlad119 <unstarkble12@gmail.com>
2025-05-07 23:15:17 +0300

    Программа удаляет из введёного числа 5 и 7 не меняя порядок цифр

---
+++ Delete 5 and 7/Delete 5 and 7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int digit = int.Parse(Console.ReadLine());
+        int result = 0;
+        int multiplier = 1;
+        while (digit > 0)
+        {
+            int currentDigit = digit % 10;
+            if (currentDigit != 5 && currentDigit != 7)
+            {
+                result += currentDigit * multiplier;
+                multiplier *= 10;
+            }
+            digit /= 10;
+        }
+        Console.WriteLine(result);
+    }
+}
