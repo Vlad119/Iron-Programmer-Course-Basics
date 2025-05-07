3c0f1fcfb51e025db95c29a6a91af4aebfbe79b1
Vlad119 <unstarkble12@gmail.com>
2025-05-07 23:24:38 +0300

    Приложение находит ближаший "красивый год" после введёного

---
+++ Beautiful year/Beautiful year/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int year = int.Parse(Console.ReadLine());
+        int d1, d2, d3, d4;
+        year++;
+        while (true)
+        {
+            d4 = year % 10;
+            d3 = year / 10 % 10;
+            d2 = year / 100 % 10;
+            d1 = year / 1000;
+            if (d4 != d3 && d4 != d2 && d4 != d1 && d3 != d2 && d3 != d1 && d2 != d1)
+            break;
+            else year++;
+        }
+        Console.Write(year);
+    }
+}
---
+++ Beautiful year/Beautiful year/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Beautiful year")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Beautiful year")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1c2b24cc-f5c7-459b-86ec-0c213ffdb8e6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
