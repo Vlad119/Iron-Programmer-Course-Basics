e667098f1fe11e953d760a4da6cf9736bd4651b9
Vlad119 <unstarkble12@gmail.com>
2025-05-07 22:39:17 +0300

    Программа выводит количество  элементов последовательности больших предыдущего элемента

---
+++ Number of elements that are greater than the previous/Number of elements that are greater than the previous/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int firstN = int.Parse(Console.ReadLine());
+        int secondN = int.Parse(Console.ReadLine());
+        int count = 0;
+        while (secondN != 0)
+        {
+            if (firstN < secondN) count++;
+            firstN = secondN;
+            secondN = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine(count);
+    }
+}
+
