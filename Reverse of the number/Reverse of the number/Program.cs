1686317e4036f667ef9a53d89261a36b36a9c4bb
Vlad119 <unstarkble12@gmail.com>
2025-05-07 22:35:26 +0300

    Программа делает реверс числа

---
+++ Reverse of the number/Reverse of the number/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            while (number > 0)
+            {
+                Console.Write(number % 10);
+                number /= 10;
+            }
+        }
+    }
