69c45f3e82588bfc68cee430e9f9a9ec65d04a82
Vlad119 <unstarkble12@gmail.com>
2025-05-07 22:15:11 +0300

    Задача определяет, сколько раз n делится на 3

---
+++ How many times is it divided/How many times is it divided/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace How_many_times_is_it_divided
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int counter = 0;
+            while (N % 3 == 0)
+            {
+                N /= 3;
+                counter++;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
