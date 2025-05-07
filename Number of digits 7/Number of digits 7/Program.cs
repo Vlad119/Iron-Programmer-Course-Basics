90698150c100d2cc22b8ce9fd02b6b5c63ed1a9f
Vlad119 <unstarkble12@gmail.com>
2025-05-07 22:59:05 +0300

    Программа находит сколько раз встречается цифра 7

---
+++ Number of digits 7/Number of digits 7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int count = 0;
+        for (int i = 1; i <= N; i++)
+        {
+            int number = i;
+            while (number > 0)
+            {
+                if (number % 10 == 7) count++;
+                number /= 10;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
---
+++ Number of digits 7/Number of digits 7/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Number of digits 7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Number of digits 7")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a7235cc0-dc49-4a29-b89e-ec3b4dbb4bc9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
