c8cee930524f7d2eeb9e273038a8a8e5a18e6c7f
Vlad119 <unstarkble12@gmail.com>
2025-05-07 23:29:12 +0300

    Приложение отображает таблицу умножения n на n

---
+++ Pythagoras Table/Pythagoras Table/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        if (n == 0) Console.WriteLine("Таблица пустая");
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                Console.Write(j * i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
