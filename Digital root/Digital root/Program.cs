9d8a8235058aae2f111c898479f216097d543544
Vlad119 <unstarkble12@gmail.com>
2025-05-07 23:19:18 +0300

    Приложение находит цифровой корень введёного числа

---
+++ Digital root/Digital root/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        while (n >= 10)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            n = sum;
+        }
+        Console.WriteLine(n);
+    }
+}
