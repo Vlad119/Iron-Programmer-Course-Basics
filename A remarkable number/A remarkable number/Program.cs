1f874c3fd24ab3e65981a2093376d02792494481
Vlad119 <unstarkble12@gmail.com>
2025-05-07 22:26:13 +0300

    Задача определяет является ли число "замечательным"

---
+++ A remarkable number/A remarkable number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int originalNumber = n;
+        int sumOfDigits = 0;
+        while (n > 0)
+        {
+            sumOfDigits += n % 10;
+            n /= 10;
+        }
+        if (sumOfDigits != 0 && originalNumber % sumOfDigits == 0)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
+
